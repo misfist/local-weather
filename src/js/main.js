ba983e14f8609d71278e6a9a8d6737b5e36d6fad
Pea <pea@misfist.com>
2015-09-07 18:36:15 -0400

    Added weather IP, location and weather info. Added unit switching and color backgrounds based on weather

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,113 @@ $(document).ready(function() {
 
     console.log('main.js');
 
-    // get ip of users
+    const APIKEY = 'ae5ad98abb4a0a8b51d04136004b8061';
+
+    // Get user IP
+    $.getJSON( 'http://ip-api.com/json/', 
+        function( data ) { 
+            // console.log( data.city );
+            // console.log( data.region );
+            // console.log( data.lon );
+            // console.log( data.lat );
+            // console.log( data.zip );
+
+            // Add city and state to page
+            $( '#location .location' ).text( data.city + ', ' + data.region );
+
+            // Default units
+            var units = 'imperial';
+
+            // Run the weather function
+            loadWeather( units );
+
+            $( '#weather-toggle' ).find( 'a#imperial' ).addClass( 'active' );
+
+            // Set fahrenheit to active by default
+            $( '#weather-toggle' ).on( 'click', 'a', function( event ) {
+
+                //console.log( '$( this )', $( this ).attr( 'id' ) );
+
+                // Set units to the value of clicked element
+                units = $( this ).attr( 'id' );
+
+                $( '#weather-toggle' ).find( 'a' ).removeClass( 'active' );
+                $( this ).addClass( 'active' );
+
+                // Run the weather function
+                loadWeather( units );
+
+
+            } );
+
+
+            // Load weather function
+            function loadWeather( units ) {
+
+                $.getJSON( 'http://api.openweathermap.org/data/2.5/weather?lat=' + data.lat + '&lon=' + data.lon + '&units=' + units +'&APPID=' + APIKEY, function( weather ) {
+                    console.log( weather );
+
+                    console.log( 'weather.main.temp', weather.main.temp );
+                    console.log( 'weather.main.temp_min', weather.main.temp_min );
+                    console.log( 'weather.main.temp_max', weather.main.temp_max );
+                    console.log( 'weather.weather[0].main', weather.weather[0].main );
+
+                    // Add weather.main as body class
+                    var weatherClass = weather.weather[0].description.split(' ').join('-');
+                    var unitsClass = ( units === 'imperial' ) ? 'wi wi-fahrenheit' : 'wi wi-celsius';
+
+                    //console.log(unitsClass);
+
+                    $( 'body' ).addClass( weatherClass );
+                    $( 'body' ).addClass( units );
+            
+                    $( '#current-temp' ).html( Math.round( weather.main.temp ) );
+                    $( '#current-temp' ).append( '<span id="units" class="wi">' );
+                    $( '#high' ).text( Math.round( weather.main.temp_max ) );
+                    $( '#low' ).text( Math.round( weather.main.temp_min ) );
+                    $( '#description .description' ).text( weather.weather[0].main );
+                    $( '#wind .wind' ).text( weather.wind.speed );
+                    $( '#units' ).addClass( unitsClass );
+
+                } );
+
+
+            }
+
+        }
+
+
+    );
+
+    // ae5ad98abb4a0a8b51d04136004b8061
 
     // look up zip code based on ip
     // https://freegeoip.net/json/ip-address
+    // api.openweathermap.org/data/2.5/weather?q=London,uk
+    // api.openweathermap.org/data/2.5/weather?lat=35&lon=139
 
     // or look up lat / lon based on ip-address
     // return data in form
     // {"ip":"104.162.71.231","country_code":"US","country_name":"United States","region_code":"NY","region_name":"New York","city":"Brooklyn","zip_code":"11215","time_zone":"America/New_York","latitude":40.662,"longitude":-73.986,"metro_code":501}
+
+    //http://ip-api.com/json
+    //     {
+    //     "as": "AS12271 Time Warner Cable Internet LLC",
+    //     "city": "Brooklyn",
+    //     "country": "United States",
+    //     "countryCode": "US",
+    //     "isp": "Time Warner Cable",
+    //     "lat": 40.6234,
+    //     "lon": -74.0288,
+    //     "org": "Time Warner Cable",
+    //     "query": "104.162.71.231",
+    //     "region": "NY",
+    //     "regionName": "New York",
+    //     "status": "success",
+    //     "timezone": "America/New_York",
+    //     "zip": "11209"
+
+    // }
 
     // get weather info based on zip code
     // api.openweathermap.org/data/2.5/weather?zip=94040,us
