4bb9685f6cd29d435e2442cb0fc52a208fbf5414
Pea <pea@misfist.com>
2018-02-20 18:20:03 -0500

    Updated API and styling.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,89 +27,23 @@
     };
   })(jQuery);
 }).call(this);
-/*jslint white: true */
+(function() {
+  var ipData;
+  var ipURL = 'https://api.ipdata.co';
+  var weatherURL = 'https://api.openweathermap.org/data/2.5/weather';
+  var weatherAPIKey = 'af7142e69f8d39cab74a170b9c4aa9bf';
 
+  $.ajax({
+    type: "GET",
+    url: ipURL,
+    dataType: "jsonp",
+    success: function (data) {
+      ipData = data;
+      //console.log(ipData);
 
-// STEPS
+      $( '#location' ).text( data.city + ', ' + data.region );
 
-// look up zip code based on ip
-// https://freegeoip.net/json/ip-address
-// api.openweathermap.org/data/2.5/weather?q=London,uk
-// api.openweathermap.org/data/2.5/weather?lat=35&lon=139
-
-// or look up lat / lon based on ip-address
-// return data in form
-// {"ip":"104.162.71.231","country_code":"US","country_name":"United States","region_code":"NY","region_name":"New York","city":"Brooklyn","zip_code":"11215","time_zone":"America/New_York","latitude":40.662,"longitude":-73.986,"metro_code":501}
-
-//http://ip-api.com/json
-//     {
-//     "as": "AS12271 Time Warner Cable Internet LLC",
-//     "city": "Brooklyn",
-//     "country": "United States",
-//     "countryCode": "US",
-//     "isp": "Time Warner Cable",
-//     "lat": 40.6234,
-//     "lon": -74.0288,
-//     "org": "Time Warner Cable",
-//     "query": "104.162.71.231",
-//     "region": "NY",
-//     "regionName": "New York",
-//     "status": "success",
-//     "timezone": "America/New_York",
-//     "zip": "11209"
-
-// }
-
-// get weather info based on zip code
-// api.openweathermap.org/data/2.5/weather?zip=94040,us
-// api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=imperial
-// api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric
-// return data in form
-// {"coord":{"lon":139,"lat":35},
-// "sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
-// "weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
-// "main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
-// "wind":{"speed":7.31,"deg":187.002},
-// "rain":{"3h":0},
-// "clouds":{"all":92},
-// "dt":1369824698,
-// "id":1851632,
-// "name":"Shuzenji",
-// "cod":200}
-
-// get icon
-// change background based on
-// https://cdnjs.cloudflare.com/ajax/libs/weather-icons/2.0.5/css/weather-icons.min.css
-
-
-
-
-$(document).ready(function() {
-
-    // console.log('main.js');
-    
-    // Instantiate Bootstrap tooltips
-    $('[data-toggle="tooltip"]').tooltip();
-
-    // API key for api.openweathermap.org
-    const APIKEY = 'af7142e69f8d39cab74a170b9c4aa9bf';
-
-    // IP lookup service URL
-    var ipUrl = 'http://ip-api.com/json/';
-
-    // Weather service URL
-    var weatherServiceUrl = 'http://api.openweathermap.org/data/2.5/weather';
-
-    // Get IP
-    $.getJSON( ipUrl )
-    .done( function( data ) {
-
-        console.log( data );
-
-        // Add city and state to page
-        $( '#location' ).text( data.city + ', ' + data.region );
-
-        // Default units (imperial = fahrenheit, metric = celsius)
+        // Default units
         var units = 'imperial';
 
         // Call get weather function
@@ -120,7 +54,7 @@ $(document).ready(function() {
 
 
         // If unit toggle is clicked
-        $( '#weather-toggle' ).on( 'click', 'a', function( event ) {
+        $( '#weather-toggle a' ).click(function( event ) {
 
             // Set units to the value of clicked element
             units = $( this ).attr( 'id' );
@@ -136,42 +70,57 @@ $(document).ready(function() {
         // Get weather data function
         function loadWeather( units ) {
 
-            $.getJSON( weatherServiceUrl, {
-                lat: data.lat,
-                lon: data.lon,
+            $.getJSON( weatherURL, {
+                lat: data.latitude,
+                lon: data.longitude,
                 units: units,
-                APPID: APIKEY
+                APPID: weatherAPIKey
             })
             .done( function( weather ) {
-
-                // console.log( weather );
 
                 // Add weather.main as body class
                 var weatherClass = weather.weather[0].description.split(' ').join('-');
                 var unitsClass = ( units === 'imperial' ) ? 'wi wi-fahrenheit' : 'wi wi-celsius';
 
                 // Get sunrise and sunset
-                var sunrise = getLocalDate( weather.sys.sunrise );
-                var sunset = getLocalDate( weather.sys.sunset );
-
-                // console.log( sunrise );
-                // console.log( sunset );
+                var sunrise = new Date( weather.sys.sunrise * 1000 ).toLocaleTimeString();
+                var sunset = new Date( weather.sys.sunset * 1000 ).toLocaleTimeString();
+                var currentConditions = weather.weather[0].main;
 
                 // Add body classes
                 $( 'body' ).addClass( weatherClass ).addClass( units );
-        
+
                 // Render mark-up and weather data
                 $( '#current-temp' ).html( Math.round( weather.main.temp ) );
                 $( '#current-temp' ).append( '<span id="units" class="wi">' );
                 $( '#high' ).text( Math.round( weather.main.temp_max ) );
                 $( '#low' ).text( Math.round( weather.main.temp_min ) );
-                $( '#description' ).text( weather.weather[0].main );
+                $( '#description' ).html( currentConditions );
                 $( '#wind .wind' ).text( weather.wind.speed );
                 $( '#units' ).addClass( unitsClass );
                 $( '#sunrise' ).find( '.localtime' ).text( sunrise );
                 $( '#sunset' ).find( '.localtime' ).text( sunset );
+                $( '#localtime' ).text( new Date().toLocaleTimeString() );
+                var weatherIconEl = $( '#current-conditions .icon' );
+                var currentConditionsLowerCase = currentConditions.toLowerCase();
 
-                $( 'span.localtime' ).localtime();
+              // Add some icons
+              // Could do this easily with CSS, but need to get the char codes
+              if( currentConditionsLowerCase.includes( 'rain' ) ) {
+                 weatherIconEl.addClass( 'wi-rain' );
+              } else if( currentConditionsLowerCase.includes( 'snow' ) ) {
+                 weatherIconEl.addClass( 'wi-snow' );
+              } else if( currentConditionsLowerCase.includes( 'cloud' ) ) {
+                 weatherIconEl.addClass( 'wi-cloudy' );
+              }
+              else if( currentConditionsLowerCase.includes( 'clear' ) || currentConditionsLowerCase.includes( 'sunny' ) ) {
+                weatherIconEl.addClass( 'wi-day-sunny' );
+              }
+              else if( currentConditionsLowerCase.includes( 'fog' ) || currentConditionsLowerCase.includes( 'mist' ) ) {
+                 weatherIconEl.addClass( 'wi-fog' );
+              } else if( currentConditionsLowerCase.includes( 'wind' ) ) {
+                 weatherIconEl.addClass( 'wi-windy' );
+              }
 
             })
             .fail(function( jqxhr, textStatus, error ) {
@@ -182,24 +131,15 @@ $(document).ready(function() {
             });
 
         };
+    }
+  });
 
-        // Get local date helper
-        function getLocalDate( utcDate ) {
-            var utcSeconds = utcDate;
-            var date = new Date(0);
-            date.setUTCSeconds( utcSeconds );
-            return date;
-        }
+  // Get local date helper
+  function getLocalDate( utcDate ) {
+      var utcSeconds = utcDate;
+      var date = new Date(0);
+      date.setUTCSeconds( utcSeconds );
+      return date;
+  }
 
-
-    })
-    .fail(function( jqxhr, textStatus, error ) {
-
-        var err = textStatus + ", " + error;
-        console.error( "Request Failed: " + err );
-
-    });
-
-
-// End
-});
+})();
